ad2381f1a042aa4383e0672f94c4ba5bbba13874
JeremyJan <jeremyjanom@gmail.com>
2019-05-15 10:25:12 -0700

    added list of groups features

--- app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/GroupActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/GroupActivity.java
@@ -11,13 +11,16 @@ import android.support.v7.widget.Toolbar;
 import android.util.AttributeSet;
 import android.view.View;
 
+import edu.uw.tacoma.tcss450.blm24.megaphone.dummy.DummyContent;
+
 /**
  * GroupActivity Activity. This creates the Group landing page
  * where you can see the groups you've joined and create a
  * new group
  */
 public class GroupActivity extends AppCompatActivity
-        implements GroupAddFragment.OnGroupAddragmentInteractionListener{
+        implements GroupAddFragment.OnGroupAddragmentInteractionListener,
+        GroupFragment.OnListFragmentInteractionListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,20 +32,29 @@ public class GroupActivity extends AppCompatActivity
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         FloatingActionButton fab = findViewById(R.id.fab);
 
+        if(findViewById(R.id.fragment_group_container) != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_group_container, new GroupFragment())
+                    .commit();
+        }
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //change this to replace once list fragment gets added
-                fragmentTransaction.add(R.id.fragment_group_container, new GroupAddFragment()) //TODO crashes
-                        .addToBackStack(null)
-                        .commit();
+                fragmentTransaction.replace(R.id.fragment_group_container, new GroupAddFragment()) //TODO crashes
+                        .addToBackStack(null).commit();
             }
         });
     }
 
     @Override
     public void onGroupAddFragmentInteraction(Uri uri) {
+
+    }
+
+    @Override
+    public void onListFragmentInteraction(DummyContent.DummyItem item) {
 
     }
 }
