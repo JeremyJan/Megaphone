79f66e2d81084e317c1c2fb7552ccd2ffceb8f0c
bbMorris <blm24@uw.edu>
2019-05-15 09:47:59 -0700

    Further name refactoring and included layout changes to fix some sizing issues.

--- app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/LoginActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/LoginActivity.java
@@ -34,7 +34,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login_page);
+        setContentView(R.layout.activity_login);
 
 
         buttonInit();
--- app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/RegisterActivity.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/RegisterActivity.java
@@ -60,7 +60,7 @@ public class RegisterActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_page);
+        setContentView(R.layout.activity_register);
 
         activityInit();
 
