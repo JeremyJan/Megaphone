6e54958d8511f4ea31e94cd5c57dd02d16177616
aaron <aaronjs5@uw.edu>
2019-06-04 21:21:40 -0700

    bw icon

--- app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/groupChat/MessageListRecyclerViewAdapter.java
+++ app/src/main/java/edu/uw/tacoma/tcss450/blm24/megaphone/groupChat/MessageListRecyclerViewAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import edu.uw.tacoma.tcss450.blm24.megaphone.groupChat.GroupMessageListFragment.OnListFragmentInteractionListener;
@@ -36,9 +37,10 @@ public class MessageListRecyclerViewAdapter extends RecyclerView.Adapter<Message
     @Override
     public void onBindViewHolder(final ViewHolder holder, int position) {
         holder.mItem = mValues.get(position);
+        String name = mValues.get(position).getName();
         holder.messageTextView.setText(mValues.get(position).getText());
-        holder.messengerTextView.setText(mValues.get(position).getName());
-
+        holder.messengerTextView.setText(name);
+        holder.messageImageView.setColorFilter(name.hashCode());
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -60,13 +62,15 @@ public class MessageListRecyclerViewAdapter extends RecyclerView.Adapter<Message
         public final View mView;
         public final TextView messageTextView;
         public final TextView messengerTextView;
+        public final ImageView messageImageView;
         public GroupMessage mItem;
 
         public ViewHolder(View view) {
             super(view);
             mView = view;
-            messageTextView = (TextView) view.findViewById(R.id.messageTextView);
-            messengerTextView = (TextView) view.findViewById(R.id.messengerTextView);
+            messageTextView = view.findViewById(R.id.messageTextView);
+            messengerTextView = view.findViewById(R.id.messengerTextView);
+            messageImageView = view.findViewById(R.id.messengerImageView);
         }
 
         @Override
